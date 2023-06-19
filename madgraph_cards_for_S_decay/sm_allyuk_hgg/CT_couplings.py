ebedce5e905b00752a2f328b43b22ef391dc78ae
David Curtin <david.r.curtin@gmail.com>
2023-06-19 11:46:11 -0400

    Add files via upload

---
+++ madgraph_cards_for_S_decay/sm_allyuk_hgg/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.31
+# Mathematica version: 12.1.1 for Mac OS X x86 (64-bit) (June 19, 2020)
+# Date: Fri 12 Aug 2022 10:10:34
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ madgraph_cards_for_S_decay/sm_allyuk_hgg/coupling_orders.py
@@ -0,0 +1,24 @@
+# This file was automatically created by FeynRules 2.3.31
+# Mathematica version: 12.1.1 for Mac OS X x86 (64-bit) (June 19, 2020)
+# Date: Fri 12 Aug 2022 10:10:34
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HIG = CouplingOrder(name = 'HIG',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HIW = CouplingOrder(name = 'HIW',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
